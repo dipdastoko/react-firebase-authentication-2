c965892d25c85e08f081e6e2cd25cf8909582094
dipdastoko <dipdastoko@gmail.com>
2022-04-13 20:47:26 +0600

    input fields added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialize';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -9,6 +10,8 @@ initializeAuthentication();
 
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const auth = getAuth();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
@@ -18,8 +21,48 @@ function App() {
       })
 
   }
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  }
+  const handleRegistration = e => {
+    console.log(email, password);
+    e.preventDefault();
+  }
   return (
-    <div className="App">
+    <div className="mx-5">
+      <form onSubmit={handleRegistration}>
+        <h3 className='text-primary'>Please Rgister</h3>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Sign in</button>
+      </form>
+      <br /><br /><br />
+      <div>-----------------------------------------</div>
+      <br /><br /><br />
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
