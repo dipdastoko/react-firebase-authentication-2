5dadfb9c633092cae91a677bc682a8b9adf8aec1
dipdastoko <dipdastoko@gmail.com>
2022-04-13 19:32:47 +0600

    google authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const googleProvider = new GoogleAuthProvider();
+
+initializeAuthentication();
+
 
 function App() {
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAv0sUhnLtMVRkRDpRzb8I17mM5bBDJbk0",
+    authDomain: "react-firebase-authentic-5f5d2.firebaseapp.com",
+    projectId: "react-firebase-authentic-5f5d2",
+    storageBucket: "react-firebase-authentic-5f5d2.appspot.com",
+    messagingSenderId: "39591927393",
+    appId: "1:39591927393:web:e3286133fd82f3875c1420"
+};
+
+export default firebaseConfig;
+
